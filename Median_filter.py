23ac79c89afecb7b81c1b387eea2738065a95f61
mxzgit <devx.mehdi@gmail.com>
2018-10-13 19:25:59 +0200

    Numpy array method

--- Median_filter.py
+++ Median_filter.py
@@ -1,26 +1,28 @@
-from PIL import Image
+import cv2
 import numpy as np
 
 
-def filter_mean(path, bloc_size):
-    img = Image.open(path)
-    img = img.convert("RGB")
-    newimg = Image.new(img.mode, img.size)
+def filter_mean(img, bloc_size):
+
+    newimg = np.zeros(shape=(img.shape[0], img.shape[1]))
     border = int(bloc_size/2)
     members = [(0, 0)]*((1+border*2)**2)
-    for i in range(border, img.size[0]-border):
+    for i in range(border, img.shape[0]-border):
         k = 0
-        for j in range(border, img.size[1]-border):
+        for j in range(border, img.shape[1]-border):
             for k in range((1+border*2)**2):
 
-                members[k] = img.getpixel(
-                    (i-border+int(k/bloc_size), j-border+int(k % bloc_size)))
+                members[k] = img[
+                    (i-border+int(k/bloc_size), j-border+int(k % bloc_size))]
 
-            newimg.putpixel((i, j), tuple(np.int_(np.median(members, axis=0))))
-
-    newimg.show()
+            newimg[i, j] = np.int_(np.median(members, axis=0))
+    return newimg
 
 
 if __name__ == "__main__":
-    path = "img.png"
-    filter_mean(path, 3)
+
+    path = 'noise_bzef.png'
+    img = cv2.imread(path, 0)
+    newimg = filter_mean(img, 5)
+    cv2.imshow('', newimg)
+    cv2.waitKey(0)
