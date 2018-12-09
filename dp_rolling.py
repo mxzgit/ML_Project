9194c7c8d71da6c08af2969455ac91da16ff605e
Muaz <muaz.sy123@gmail.com>
2018-12-09 15:38:13 +0100

    add compute train_test dis code

--- dp_rolling.py
+++ dp_rolling.py
@@ -5,19 +5,23 @@
 
 _MAX = 61
 _OO = (1 << 60)
-
+import sys
 dis = [[0 for _ in range(8)] for _ in range(8)]
 for i in range(8):
 	for j in range(8):
 		dis[i][j] = min(abs(i - j), min(i, j) + 8 - max(i, j)) * 1/2
-
+print('coco')
 # allocate memory once
 _DP_ROLL = [[_OO for x in range(_MAX)] for y in range(2)]
-
+c = 0
 def dp_rolling_ed(s1, s2):
+	global c
 	sz1 = len(s1)
 	sz2 = len(s2)
-
+	c += 1
+	if c%1000 == 0:
+		print(c//1000)
+		sys.stdout.flush()
 	for i in range(sz2 + 1):
 		_DP_ROLL[0][i] = i
 
